a96b534cf1dcfb975f0352c8b03c26aa36131869
zenghyun <pipo36@naver.com>
2023-05-24 22:59:24 +0900

    Commit Chart program

---
+++ chart_program/src/index.js
@@ -0,0 +1,15 @@
+// import ReactDOM from 'react-dom/client';
+
+// import './index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App2 from './App2';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App2 />);
